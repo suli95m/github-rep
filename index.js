15074dff56d77dd99c160d7ae1f514f2853e4722
Suli95m <baisugurov@hotmail.com>
2023-06-20 13:12:57 +0300

    new

--- index.js
+++ index.js
@@ -1,14 +1,13 @@
-function solution(input, markers) {
-  let res = ''
-  const newarr = []
-  const formatInput = input.trim()
-  const arr = formatInput.split('\')
-  for (let i = 0; i < arr.length; i++) {
-    res = ''
-    for (let k = 0; !arr[i][k].includes(markers); k++) {
-        res += arr[i][k]
-    }
-    newarr.push(res)
-  }
-  return newarr.join('\')
-};
+let firstName = 'Sulim'
+let lastName = 'Baisugurov'
+const myObj = {
+	firstName,
+	lastName,
+	sayHello() {
+		return 'Hello, world!'
+	},
+}
+
+//console.log(myObj.sayHello())
+myObj.firstName = 'Surkho'
+console.log(firstName)
